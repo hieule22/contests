94e704a2bd53dab77c6ba9ae99af24605f9c081f
Hieu Le <trunghieule94@gmail.com>
2016-11-27 13:28:43 -0600

    Codeforces Round 382 Div 1

---
+++ codeforces/R382D1/A.java
@@ -0,0 +1,68 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author Hieu Le
+ */
+public class A {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        TaskA solver = new TaskA();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskA {
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+            long numPlayers = in.nextLong();
+            long answer = 0, lastFib = 1, currentFib = 2;
+            while (currentFib <= numPlayers) {
+                ++answer;
+                currentFib += lastFib;
+                lastFib = currentFib - lastFib;
+            }
+            out.println(answer);
+        }
+
+    }
+
+    static class InputReader {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+        private static final int BUFFER_SIZE = 32768;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), BUFFER_SIZE);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+    }
+}
---
+++ codeforces/R382D1/B.java
@@ -0,0 +1,79 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author Hieu Le
+ */
+public class B {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        TaskB solver = new TaskB();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+            int income = in.nextInt();
+            if (isPrime(income)) {
+                out.println(1);
+            } else if (income % 2 == 0 || isPrime(income - 2)) {
+                out.println(2);
+            } else {
+                out.println(3);
+            }
+        }
+
+        private boolean isPrime(int n) {
+            if (n < 2)
+                return false;
+            int limit = (int) Math.sqrt(n);
+            for (int i = 2; i <= limit; ++i)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+
+    }
+
+    static class InputReader {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+        private static final int BUFFER_SIZE = 32768;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(
+                    new InputStreamReader(stream), BUFFER_SIZE);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
